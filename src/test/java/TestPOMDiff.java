83b5ee15e9c45fc34b69cd8158a810327eb37ad2
Mahesh <pmahesh2014@gmail.com>
2022-11-04 22:52:17 +0530

    updated xpath node fiif file

--- src/test/java/TestPOMDiff.java
+++ src/test/java/TestPOMDiff.java
@@ -1,56 +1,73 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TestPOMDiff {
 
     public static void main(String args[]){
 
         try {
-            String firstValue = null;
-            String secondValue = null;
-
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(true);
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            Document doc = db.parse(new File(System.getProperty("user.dir")+"/src/test/data/testpom1.xml"));
-            XPathFactory xPathFactory = XPathFactory.newInstance();
-            XPath xpath = xPathFactory.newXPath();
-
-            //need to update the first xapath
-            XPathExpression expr = xpath.compile("//project/modelVersion");
-
-            Object exprValue = expr.evaluate(doc, XPathConstants.STRING);
-
-            if (exprValue != null) {
-                firstValue = exprValue.toString();
-            }
-
-            doc = db.parse(new File(System.getProperty("user.dir")+"/src/test/data/testpom2.xml"));
-
-            //need to replace the second xpath
-            expr = xpath.compile("//dependencies/dependency/version");
-            exprValue = expr.evaluate(doc, XPathConstants.STRING);
-
-            if (exprValue != null) {
-                secondValue = exprValue.toString();
-            }
-
-            if (firstValue != null && secondValue != null) {
-                System.out.println(firstValue);
-                System.out.println(secondValue);
-
-
-            }
+           String xpathOfversionInFile1 = getXpath("testpom1locations","version");
+            String xpathOfversionInFile2 = getXpath("testpom2locations","version");
+            printValues(xpathOfversionInFile1,xpathOfversionInFile2);
+            String xpathOfArtifactIdInFile1 = getXpath("testpom1locations","artifactId");
+            String xpathOfArtifactIdInFile2 = getXpath("testpom2locations","artifactId");
+            printValues(xpathOfArtifactIdInFile1,xpathOfArtifactIdInFile2);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static String getXpath(String parentKey,String childKey) throws FileNotFoundException {
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(System.getProperty("user.dir")+"/src/test/data/xpathlocators.json")) {
+            Object obj = jsonParser.parse(reader);
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONObject jsonChildObject = (JSONObject)jsonObject.get(parentKey);
+            String path = (String) jsonChildObject.get(childKey);
+           // System.out.println(path);
+            return path;
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void printValues(String xpathinfile1,String xpathinfile2) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+        String firstValue = null;
+        String secondValue = null;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new File(System.getProperty("user.dir")+"/src/test/data/testpom1.xml"));
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+        //need to update the first xapath
+        XPathExpression expr = xpath.compile(xpathinfile1);
+        Object exprValue = expr.evaluate(doc, XPathConstants.STRING);
+        if (exprValue != null) {
+            firstValue = exprValue.toString();
+        }
+        doc = db.parse(new File(System.getProperty("user.dir")+"/src/test/data/testpom2.xml"));
+        //need to replace the second xpath
+        expr = xpath.compile(xpathinfile2);
+        exprValue = expr.evaluate(doc, XPathConstants.STRING);
+        if (exprValue != null) {
+            secondValue = exprValue.toString();
+        }
+        if (firstValue != null && secondValue != null) {
+            System.out.println(firstValue);
+            System.out.println(secondValue);
         }
     }
 }
